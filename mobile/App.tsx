6d8aa190cf7d7fb7d29ed4ff67b1ab38429db401
enoquetembe <enoquetembetembe10@gmail.com>
2022-09-14 17:50:05 -0700

    mobile environment

---
+++ mobile/App.tsx
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+}
+
+const Button = (props: ButtonProps) => {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World </Text>
+
+      <StatusBar style="auto" backgroundColor='green' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});
